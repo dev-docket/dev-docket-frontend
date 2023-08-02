29f8408ccf59895fcf501bd4d2bfe652137f5b5a
Błażej Domagała <petitoff@outlook.com>
2023-08-02 20:10:38 +0200

    use generic handleError and validate if userId and token is not null

--- src/hooks/tasks/useFetchTasks.ts
+++ src/hooks/tasks/useFetchTasks.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../storeHook";
 import { addTasks } from "../../store/slices/taskSlice";
 import axios from "axios";
-import { toast } from "react-toastify";
+import { handleError } from "../../utils/handleError";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -10,15 +10,17 @@ export const useFetchTasks = () => {
   const userId = useAppSelector((state) => state.user.user?.id);
   const token = useAppSelector((state) => state.auth.token);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-
+  const [error, setError] = useState<string | null>(null);
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const fetchTasks = async () => {
       setIsLoading(true);
+      setError(null);
       try {
+        if (!userId || !token) throw new Error("Please login first");
+
         const response = await axios.get(`${apiUrl}/users/${userId}/tasks`, {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -29,15 +31,18 @@ export const useFetchTasks = () => {
         }
 
         const data = await response.data;
-        // const tasks = data.map((task: { title: string }) => task.title);
         dispatch(addTasks(data));
       } catch (err) {
-        setError(err.message);
-        toast.error(err.message);
+        if (err instanceof Error) {
+          setError(err.message);
+        }
+
+        handleError(err);
       }
       setIsLoading(false);
     };
 
+    if (!userId) return;
     fetchTasks();
   }, [dispatch, token, userId]);
 
