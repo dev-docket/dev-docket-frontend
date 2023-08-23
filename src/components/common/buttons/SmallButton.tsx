0dea1b3d48c18cce2f8d4d549dd10c381e2d129f
Błażej Domagała <petitoff@outlook.com>
2023-08-23 17:45:57 +0200

    fix: button have not execution after click

--- src/components/common/buttons/SmallButton.tsx
+++ src/components/common/buttons/SmallButton.tsx
@@ -21,7 +21,10 @@ export const SmallButton = ({ redirectPath, title, onClick }: Props) => {
           {title}
         </Link>
       ) : (
-        <button className="flex transition-all items-center min-w-full justify-center px-3 py-2 font-medium text-white bg-indigo-500 rounded-md hover:bg-gray-700 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+        <button
+          onClick={onClick}
+          className="flex transition-all items-center min-w-full justify-center px-3 py-2 font-medium text-white bg-indigo-500 rounded-md hover:bg-gray-700 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
           {title}
         </button>
       )}
