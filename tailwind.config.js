af19edcc4ee2bbce7dc82f0e355d76696d70f92f
Błażej Domagała <petitoff@outlook.com>
2023-08-29 13:53:17 +0200

    added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
         "dark-background": "#212121",
         "secondary-background": "#383838",
         "header-background": "#292929",
+
+        "border-dark-primary": "#30363d",
+        "button-hover-dark": "#21262c",
+
+        icon: {
+          gray: "#7d8590",
+        },
       },
     },
   },
