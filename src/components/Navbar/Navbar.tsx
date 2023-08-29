8aa4b5ebd9a9f7131590f091bb749c8614455d73
Błażej Domagała <petitoff@outlook.com>
2023-08-29 15:44:10 +0200

    created and implemented Navbar and DropdownMenu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useLogout } from "../../hooks/auth/useLogout";
 import { useAppSelector } from "../../hooks/storeHook";
 import { SmallButton } from "../common/buttons/SmallButton";
+import { DropdownMenu } from "./DropdownMenu";
 
 export const Navbar = () => {
   const userIsLoggedIn = useAppSelector((state) => state.auth.token !== null);
@@ -11,7 +12,21 @@ export const Navbar = () => {
   return (
     <nav className="z-20 border-b border-b-border-dark-primary bg-header-background p-4 text-white">
       <div className="flex justify-between">
-        <div className="flex space-x-4">
+        <div className="flex w-full items-center justify-between space-x-4">
+          <div>
+            <Link
+              to="/dashboard"
+              className="px-4 py-2 hover:rounded-md hover:bg-icon-gray hover:bg-opacity-20"
+            >
+              Dasboard
+            </Link>
+            {/* <button className="p-2 hover:rounded-md hover:bg-icon-gray hover:bg-opacity-20">
+              Projects <ArrowDropDownSharp />
+            </button> */}
+
+            <DropdownMenu />
+          </div>
+
           <>
             {userIsLoggedIn ? (
               <SmallButton
@@ -22,11 +37,6 @@ export const Navbar = () => {
             ) : (
               <SmallButton redirectPath="/login" title="Login" />
             )}
-
-            <div className="flex items-center justify-center p-2">
-              Dashboard
-            </div>
-            <div className="flex items-center justify-center">Projects</div>
           </>
         </div>
       </div>
