adda5ec22657a000b6ce3bb987632668de68b4de
Błażej Domagała <petitoff@outlook.com>
2023-09-15 13:03:59 +0200

    responsive menu sidebar in project page

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -5,11 +5,16 @@ import { useNavigate, useParams } from "react-router-dom";
 import { fetchTeamsByProjectSlug } from "../store/slices/actions/team";
 import { TeamCard } from "../features/Project/TeamCard";
 import { SmallButton } from "../components/common/buttons/SmallButton";
+import { CreateNewTeamModal } from "../features/Project/CreateNewTeam/CreateNewTeamModal";
 
 export const Project = () => {
   const { teams, loading } = useAppSelector((state) => state.team);
+  const isSidebarOpen = useAppSelector(
+    (state) => state.globalSettings.isMenuSidebarOpen,
+  );
 
-  const [, setIsCreateNewTeamModalOpen] = useState(false);
+  const [isCreateNewTeamModalOpen, setIsCreateNewTeamModalOpen] =
+    useState(false);
 
   const { projectSlug } = useParams<{
     projectSlug: string;
@@ -32,45 +37,51 @@ export const Project = () => {
   return (
     <div className="flex h-screen flex-col bg-dark-background text-white">
       <Navbar />
-      <div className="flex justify-end">
-        <div className="mt-4 w-[80%] px-4 transition-all max-md:w-full">
-          <div className="flex w-full justify-between">
-            <div>
-              <h1 className="text-2xl">Your work</h1>
-              <h2 className="text-sm">
-                You can find your teams here in project
-              </h2>
-            </div>
-            <div className="w-[8rem]">
-              <SmallButton
-                title="Create new team"
-                onClick={() => setIsCreateNewTeamModalOpen(true)}
-              />
-            </div>
+      <div
+        className={`mt-4  ${
+          isSidebarOpen ? "ml-[20%]" : "w-[100%]"
+        } px-4 transition-all max-md:w-full`}
+      >
+        <div className="flex w-full justify-between">
+          <div>
+            <h1 className="text-2xl">Your work</h1>
+            <h2 className="text-sm">You can find your teams here in project</h2>
           </div>
+          <div className="w-[8rem]">
+            <SmallButton
+              title="Create new team"
+              onClick={() => setIsCreateNewTeamModalOpen(true)}
+            />
+          </div>
+        </div>
 
-          <div className="mt-7">
-            <div className="mt-10 flex gap-3 overflow-auto pb-4">
-              {loading == "succeeded" ? (
-                teams.map((team) => (
-                  <div key={team.id} className="w-[13rem]">
-                    <TeamCard
-                      team={team}
-                      onNavigateToTeamPage={handleNavigateToTeamPage}
-                    />
-                  </div>
-                ))
-              ) : (
-                <div
-                  className="inline-block h-6 w-6 animate-spin rounded-full border-[3px] border-current border-t-transparent pl-2 text-blue-600"
-                  role="status"
-                  aria-label="loading"
-                />
-              )}
-            </div>
+        <div className="mt-7">
+          <div className="mt-10 flex gap-3 overflow-auto pb-4">
+            {loading == "succeeded" ? (
+              teams.map((team) => (
+                <div key={team.id} className="w-[13rem]">
+                  <TeamCard
+                    team={team}
+                    onNavigateToTeamPage={handleNavigateToTeamPage}
+                  />
+                </div>
+              ))
+            ) : (
+              <div
+                className="inline-block h-6 w-6 animate-spin rounded-full border-[3px] border-current border-t-transparent pl-2 text-blue-600"
+                role="status"
+                aria-label="loading"
+              />
+            )}
           </div>
         </div>
       </div>
+
+      {isCreateNewTeamModalOpen && (
+        <CreateNewTeamModal
+          closeModal={() => setIsCreateNewTeamModalOpen(false)}
+        />
+      )}
     </div>
   );
 };
