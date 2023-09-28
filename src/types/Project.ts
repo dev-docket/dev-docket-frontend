a36f42f4f5e353186618ee106e9239bde205d0b0
Błażej Domagała <petitoff@outlook.com>
2023-09-28 10:14:57 +0200

    added interface ProjectMember and ProjectInvitation

--- src/types/Project.ts
+++ src/types/Project.ts
@@ -1,6 +1,24 @@
+import { User } from "./User";
+
 export interface Project {
   id?: number;
   name?: string;
   description?: string;
   slug?: string;
+}
+
+export interface ProjectMember {
+  userId?: number;
+  projectId?: number;
+  role?: string;
+  user?: User;
+}
+
+export interface ProjectInvitation {
+  token?: string;
+  projectId?: number;
+  creatorId?: number;
+  projectName?: string;
+  user: User;
+  // expiry?:
 }
