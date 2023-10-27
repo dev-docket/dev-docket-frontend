90658a0eb4119c4f66b52f1699b78d976eba2de9
Błażej Domagała <petitoff@outlook.com>
2023-10-27 11:40:37 +0200

    after logout, navigate to login route

--- src/hooks/auth/useLogout.ts
+++ src/hooks/auth/useLogout.ts
@@ -4,9 +4,11 @@ import { removeTasks } from "../../store/slices/taskSlice";
 import { removeUser } from "../../store/slices/userSlice";
 import { useAppDispatch } from "../storeHook";
 import { removeProjects } from "../../store/slices/projectSlice";
+import { useNavigate } from "react-router-dom";
 
 export const useLogout = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const logoutUser = () => {
     dispatch(removeToken());
@@ -15,6 +17,7 @@ export const useLogout = () => {
     dispatch(removeProjects());
 
     toast.success("You have successfully logged out", { autoClose: 1000 });
+    navigate("/login");
   };
 
   return { logoutUser };
