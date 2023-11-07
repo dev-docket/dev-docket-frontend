9f48db9db47ed883b6757e2503affa60d503fbb7
Błażej Domagała <petitoff@outlook.com>
2023-11-08 00:11:21 +0100

    Add priority icon to "No priority" option in task details sidebar.

--- src/features/KanbanBoard/TaskDetailsSidebar/RightContainer.tsx
+++ src/features/KanbanBoard/TaskDetailsSidebar/RightContainer.tsx
@@ -160,7 +160,18 @@ export const RightContainer = () => {
               label="No priority"
               options={[
                 <div className="flex items-center gap-2">
-                  <span>...</span> No priority
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M16 12a2 2 0 0 1 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2m-6 0a2 2 0 0 1 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2m-6 0a2 2 0 0 1 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2Z"
+                    />
+                  </svg>
+                  No priority
                 </div>,
                 <div className="flex items-center gap-2">
                   <WarningAmber /> Urgent
