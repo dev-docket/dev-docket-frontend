3f57c040c94162a5c1c8b540058ccbfd06a00318
Błażej Domagała <petitoff@outlook.com>
2023-11-08 00:06:59 +0100

    Add TaskPriority enum and displayPriority function

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -3,6 +3,7 @@ export interface Task {
   name: string;
   description?: string;
   status: TaskStatus;
+  priority: TaskPriority;
 }
 
 export type TaskStatus = "BACKLOG" | "TODO" | "IN_PROGRESS" | "DONE";
@@ -13,6 +14,19 @@ const StatusEnum = {
   IN_PROGRESS: "IN_PROGRESS",
   DONE: "DONE",
 };
+
+export enum TaskPriority {
+  No_Priority = "No Priority",
+  URGENT = "URGENT",
+  HIGH = "HIGH",
+  MEDIUM = "MEDIUM",
+  LOW = "LOW",
+}
+
+export function getPriorityByIndex(index: number): TaskPriority | undefined {
+  const priorities = Object.values(TaskPriority);
+  return priorities[index];
+}
 
 export function displayStatus(status?: TaskStatus) {
   if (!status) {
@@ -30,5 +44,26 @@ export function displayStatus(status?: TaskStatus) {
       return "DONE";
     default:
       return status;
+  }
+}
+
+export function displayPriority(priority?: TaskPriority) {
+  if (!priority) {
+    return "Unknown";
+  }
+
+  switch (priority) {
+    case TaskPriority.No_Priority:
+      return "No Priority";
+    case TaskPriority.LOW:
+      return "Low";
+    case TaskPriority.MEDIUM:
+      return "Medium";
+    case TaskPriority.HIGH:
+      return "High";
+    case TaskPriority.URGENT:
+      return "Urgent";
+    default:
+      return priority;
   }
 }
