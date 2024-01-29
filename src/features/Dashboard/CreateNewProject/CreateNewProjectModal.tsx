34221a1586de98670f4b50ea95bb83a8209eb07b
Błażej Domagała <petitoff@outlook.com>
2024-01-29 12:34:52 +0100

    Update z-index value in CreateNewProjectModal component

--- src/features/Dashboard/CreateNewProject/CreateNewProjectModal.tsx
+++ src/features/Dashboard/CreateNewProject/CreateNewProjectModal.tsx
@@ -35,7 +35,7 @@ export const CreateNewProjectModal = ({ closeModal }: Props) => {
   };
 
   return (
-    <div className="absolute z-30 flex h-screen w-screen bg-secondary-background max-md:flex-col">
+    <div className="absolute z-50 flex h-screen w-screen bg-secondary-background max-md:flex-col">
       <div className="flex h-full w-1/4 flex-col bg-dark-background p-4 max-md:h-1/2 max-md:w-full">
         <Close
           onClick={closeModal}
