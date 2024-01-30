1623ab89f0535029335ea0d1cb87748a3d78a829
Błażej Domagała <petitoff@outlook.com>
2024-01-30 16:02:56 +0100

    Add member indicator in project settings

--- src/features/Project/ProjectSettings/Access.tsx
+++ src/features/Project/ProjectSettings/Access.tsx
@@ -10,6 +10,7 @@ export const Access = ({ projectMembers }: Props) => {
       <h3 className="text-sm font-medium">Project members</h3>
       {projectMembers.map((member, index) => (
         <div key={index} className="mt-2 flex items-center">
+          <span className="mr-2 inline-block h-2 w-2 rounded-full bg-highlight-primary align-middle"></span>
           <span className="ml-2">{member.user?.username}</span>
         </div>
       ))}
